b4e3372a3236514649775bffd4d327846b7d24f7
정태승 <hi563@g.skku.edu>
2023-06-20 11:05:07 +0900

    My First Spring Boot TODO App

---
+++ src/main/java/com/springboot/myfirstwebapptodo/MyfirstwebappTodoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.myfirstwebapptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebappTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebappTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/myfirstwebapptodo/MyfirstwebappTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.myfirstwebapptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebappTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
